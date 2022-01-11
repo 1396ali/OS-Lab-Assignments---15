38bccc05382216810c34e8157f40ac02597defa9
Ali Poorabbas <ali.saba1396@gmail.com>
2022-01-11 07:45:41 +0330

    second commit

---
+++ INSERT.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO Customers (id,name,city,county)
+VALUES (1, "Ali", "Mashhad", "Iran"), (2, "Sogand", "USA", "California"), 
+(3, "Pepe", "Rio", "Brazil") ;
+ 
+ 
+INSERT INTO Products (id,name,price,count)
+VALUES (1, "Fish", 110000, 10), (2, "Apple", 22000, 20), (3, "Banana", 33000, 30) ;
